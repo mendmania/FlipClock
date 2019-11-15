36cbed7756cb33748218422a4f0c237780aa1de3
Mendim Arifaj <mendmania@gmail.com>
2019-11-15 10:37:45 +0100

    Add al-al.js
    Adding Albanian language translation to FlipClock

---
+++ src/flipclock/js/lang/al-al.js
@@ -0,0 +1,27 @@
+(function($) {
+
+    /**
+     * FlipClock Albanian Language Pack
+     *
+     * This class will be used to translate tokens into the Albanian language.
+     * D.M
+     */
+
+    FlipClock.Lang.Arabic = {
+
+      'years'   : 'Vitet',
+      'months'  : 'Muaj',
+      'days'    : 'Ditë',
+      'hours'   : 'Orë',
+      'minutes' : 'Minuta',
+      'seconds' : 'Sekonda'
+
+    };
+
+    /* Create various aliases for convenience */
+
+    FlipClock.Lang['al']      = FlipClock.Lang.Albanian;
+    FlipClock.Lang['al-al']   = FlipClock.Lang.Albanian;
+    FlipClock.Lang['albaninan']  = FlipClock.Lang.Albanian;
+
+}(jQuery));
